e04b93ca11068a62e6f7a3fcbd0f409c83401fcc
Mobin <mobthomas@gmail.com>
2020-02-19 15:43:13 +1100

    uncommented options for terrraform

--- scripts/option1.sh
+++ scripts/option1.sh
@@ -8,7 +8,7 @@ echo -e $prefix
 read -p "UIserver vm password The supplied password must be between 6-72 characters  \
 long and must satisfy azure password complexity requirements: " password \n
 export password
-IFS= read -r -p "Enter ssh key for the vm: " keypath \n
+IFS= read -r -p "Enter ssh key for the vm  /home/"username"/.ssh/id_rsa.pub: " keypath \n
 export keypath
 echo -e $keypath
 #read -p "port, which need to be exposed for the application: " aport \n
@@ -22,11 +22,11 @@ must satisfy azure password complexity requirements: " psqldbpassword \n
 export psqldbpassword
 ./scripts/conf.toml.sh  $psqldbname $psqldbpassword > /tmp/conf.toml
 #rm -rf terraform.tfstate terraform.tfstate.backup
-#terraform init 
+terraform init 
 sleep 10       # change this to more time if initializing for first time
-#terraform plan -var="prefix=$prefix" -var="location=$location" -var="password=$password" -var="psqldbpassword=$psqldbpassword" -var="psqldbname=$psqldbname"
+terraform plan -var="prefix=$prefix" -var="location=$location" -var="password=$password" -var="psqldbpassword=$psqldbpassword" -var="psqldbname=$psqldbname"
 #echo $prefix n $location
-#terraform apply -auto-approve -var="prefix=$prefix" -var="location=$location" -var="password=$password" -var="psqldbpassword=$psqldbpassword" -var="psqldbname=$psqldbname"
+terraform apply -auto-approve -var="prefix=$prefix" -var="location=$location" -var="password=$password" -var="psqldbpassword=$psqldbpassword" -var="psqldbname=$psqldbname"
 
 serv0=$(terraform output vm0_puplic_ip0)
 export serv0
