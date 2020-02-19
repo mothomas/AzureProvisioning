7f5b2bc9d989fb84e685474a6c4c112de7bacae1
Mobin <mobthomas@gmail.com>
2020-02-19 14:32:38 +1100

    clearing main repo keeping only docs

--- scripts/option1.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-rm -rf terraform.tfstate terraform.tfstate.backup
-terraform init 
-terraform plan -var="prefix=$prefix" -var="location=$location" -var="sshkey=$sshkey"
-terraform apply -auto-approve -var="prefix=$prefix" -var="location=$location" -var="sshkey=$ssh-key"
