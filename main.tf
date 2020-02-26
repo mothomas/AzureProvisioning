d2bf320e45908d6334538f15e05f4dcc87acb4b9
Mobin <mobthomas@gmail.com>
2020-02-26 13:37:53 +1100

    terraform azurerm version pinned to 1.44

--- main.tf
+++ main.tf
@@ -1,6 +1,6 @@
 #provider "azurerm" {
 # Whilst version is optional, we /strongly recommend/ using it to pin the version of the Provider being used
-#  version = "=1.44.0"
+  version = "=1.44.0"
 #}  
 
 resource "azurerm_resource_group" "UIserver" {
